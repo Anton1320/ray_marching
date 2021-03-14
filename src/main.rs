6af56e88ef46e960f9f3092b7e49ccc4a5037202
Anton1320 <56134229+Anton1320@users.noreply.github.com>
2021-03-14 23:57:26 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,24 @@ extern crate ray_marching;
 extern crate image;
 use piston_window::*;
 use ray_marching::*;
-//use std::time::Instant;
+
+struct SuperCube {
+    sphere: Sphere,
+    cube: Box,
+    color: Vector3,
+}
+
+impl Figure for SuperCube {
+    fn get_distance(&self, point: Vector3) -> f32 {
+        self.cube.get_distance(point).max(-self.sphere.get_distance(point))
+    }
+}
 
 fn main() {
     let cam = Camera {
         pos: Vector3::new(0., 0., 0.),
         screen_size: Vector3::new(4., 4., 0.),
-        screen_resolution: (200, 200),
+        screen_resolution: (100, 100),
         dist_to_screen: 2.,
         vector_to_screen: Vector3::new(1., 0., 0.),
         angle_vector_x: Vector3::new(0., 0., 1.),
@@ -19,44 +30,42 @@ fn main() {
     
     let sphere = Sphere {
         center: Vector3::new(3.5, 0., 0.),
-        r: 1.95,
+        r: 1.9,
         color: Vector3::new(255., 0., 0.),
     };
 
-    let mut cube = Box::new
+    let cube = Box::new
     (
         Vector3::new(3.5, 0., 0.),
         Vector3::new(0., 0., 0.),
         Vector3::new(1.4, 1.4, 1.4),
         Vector3::new(255., 255., 1.)
     );
-
+    let mut super_cube = SuperCube {
+        cube: cube,
+        sphere: sphere,
+        color: Vector3::new(255., 255., 1.),
+    };
     let render_vectors = cam.get_render_vectors();
 
-    //let mut pixels: Vec<Vec<f32>> = vec![vec![0.; cam.screen_resolution.1]; cam.screen_resolution.0];
     let mut pixels = image::ImageBuffer::from_pixel(cam.screen_resolution.0 as u32, cam.screen_resolution.1 as u32, image::Rgba([0,0,0, 255]));
-    //println!("{}", 1);
     let mut window: PistonWindow =
         WindowSettings::new("Ray marching", [cam.screen_resolution.0 as u32, cam.screen_resolution.1 as u32])
         .exit_on_esc(true).build().unwrap();
     while let Some(event) = window.next() {
-        cube.rotation.x += 1./40.;
-        cube.rotation.y += 1./45.;
-        cube.rotation.z += 1./35.;
+        super_cube.cube.rotation.x += 1./40.;
+        super_cube.cube.rotation.y += 1./45.;
+        super_cube.cube.rotation.z += 1./35.;
         //sphere.center.z += 0.1;
-        //let now = Instant::now();
         for i in 0..cam.screen_resolution.0 {
             for j in 0..cam.screen_resolution.1 {
                 let mut ray = Vector3::new(0., 0., 0.);
                 pixels.put_pixel(i as u32, j as u32, image::Rgba([0, 0, 0, 255]));
                 for _k in 0..50 {
                     //ray.z = (ray.z-2.)%4.+2.;
-                    let dist = cube.get_distance(cam.pos+ray).max(-sphere.get_distance(cam.pos+ray));
+                    let dist = super_cube.get_distance(cam.pos+ray);
                     if dist < 0.01 {
-                        let a: Vector3;
-                        if -sphere.get_distance(cam.pos+ray) > cube.get_distance(cam.pos+ray)
-                            {a = cube.color*((cube.get_normal(cam.pos+ray)*(light-cam.pos-ray).norm()).powf(2.)+0.2);}
-                        else {a = cube.color*((sphere.get_normal(cam.pos+ray)*(-1.)*(light-cam.pos-ray).norm()).powf(2.)+0.2);}
+                        let a = super_cube.color*((super_cube.get_normal(cam.pos+ray)*(-1.)*(light-cam.pos-ray).norm()).powf(2.)+0.2);
                         pixels.put_pixel(i as u32, j as u32, image::Rgba([a.x as u8, a.y as u8, a.z as u8, 255]));
                         break;
                     } 
@@ -65,8 +74,6 @@ fn main() {
                 }
             }
         }
-        //let new = Instant::now();
-        //println!("coutnt {:?}", new.duration_since(now));
         let tex = Texture::from_image(
             &mut window.create_texture_context(),
             &pixels,
@@ -77,7 +84,5 @@ fn main() {
             clear([1.0; 4], graphics);
             image(&tex, context.transform, graphics);
         });
-        //let now = Instant::now();
-        //println!("draw {:?}", now.duration_since(new));
     }
 }
