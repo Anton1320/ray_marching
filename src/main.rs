6a1145a3ea0439b12bf1fd07e024a07157eab6db
Anton1320 <56134229+Anton1320@users.noreply.github.com>
2021-03-14 21:22:42 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ extern crate ray_marching;
 extern crate image;
 use piston_window::*;
 use ray_marching::*;
+//use std::time::Instant;
 
 fn main() {
     let cam = Camera {
         pos: Vector3::new(0., 0., 0.),
         screen_size: Vector3::new(4., 4., 0.),
-        screen_resolution: (100, 100),
+        screen_resolution: (200, 200),
         dist_to_screen: 2.,
         vector_to_screen: Vector3::new(1., 0., 0.),
         angle_vector_x: Vector3::new(0., 0., 1.),
@@ -17,15 +18,18 @@ fn main() {
     let light = Vector3::new(0., 0., 2.);
     
     let sphere = Sphere {
-        center: Vector3::new(3.5, 0., -5.),
-        r: 1.,
+        center: Vector3::new(3.5, 0., 0.),
+        r: 1.95,
         color: Vector3::new(255., 0., 0.),
     };
 
-    let mut cube = Box::new(Vector3::new(3.5, 0., 0.),
-     Vector3::new(0., 3.14159265/5., 3.14159265/4.),
-      Vector3::new(1., 1., 1.),
-       Vector3::new(255., 255., 1.));
+    let mut cube = Box::new
+    (
+        Vector3::new(3.5, 0., 0.),
+        Vector3::new(0., 0., 0.),
+        Vector3::new(1.4, 1.4, 1.4),
+        Vector3::new(255., 255., 1.)
+    );
 
     let render_vectors = cam.get_render_vectors();
 
@@ -39,23 +43,30 @@ fn main() {
         cube.rotation.x += 1./40.;
         cube.rotation.y += 1./45.;
         cube.rotation.z += 1./35.;
-        //sphere.center.z += 1.;
+        //sphere.center.z += 0.1;
+        //let now = Instant::now();
         for i in 0..cam.screen_resolution.0 {
             for j in 0..cam.screen_resolution.1 {
                 let mut ray = Vector3::new(0., 0., 0.);
                 pixels.put_pixel(i as u32, j as u32, image::Rgba([0, 0, 0, 255]));
                 for _k in 0..50 {
-                    let dist = cube.get_distance(cam.pos+ray);
+                    //ray.z = (ray.z-2.)%4.+2.;
+                    let dist = cube.get_distance(cam.pos+ray).max(-sphere.get_distance(cam.pos+ray));
                     if dist < 0.01 {
-                       let a = cube.color*((cube.get_normal(cam.pos+ray)*(light-cam.pos-ray).norm()).powf(2.)+0.2);
-                       pixels.put_pixel(i as u32, j as u32, image::Rgba([a.x as u8, a.y as u8, a.z as u8, 255]));
-                       break;
+                        let a: Vector3;
+                        if -sphere.get_distance(cam.pos+ray) > cube.get_distance(cam.pos+ray)
+                            {a = cube.color*((cube.get_normal(cam.pos+ray)*(light-cam.pos-ray).norm()).powf(2.)+0.2);}
+                        else {a = cube.color*((sphere.get_normal(cam.pos+ray)*(-1.)*(light-cam.pos-ray).norm()).powf(2.)+0.2);}
+                        pixels.put_pixel(i as u32, j as u32, image::Rgba([a.x as u8, a.y as u8, a.z as u8, 255]));
+                        break;
                     } 
-                    else if ray.length() > 5. {break;}
+                    else if ray.length() > 10. {break;}
                     ray = ray+(render_vectors[i][j])*dist;
                 }
             }
         }
+        //let new = Instant::now();
+        //println!("coutnt {:?}", new.duration_since(now));
         let tex = Texture::from_image(
             &mut window.create_texture_context(),
             &pixels,
@@ -66,5 +77,7 @@ fn main() {
             clear([1.0; 4], graphics);
             image(&tex, context.transform, graphics);
         });
+        //let now = Instant::now();
+        //println!("draw {:?}", now.duration_since(new));
     }
 }
