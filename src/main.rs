6e1684d33160d9f90aec40a90b427b13ac60a139
Anton1320 <56134229+Anton1320@users.noreply.github.com>
2021-03-16 17:34:15 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,14 @@ impl Figure for SuperSphere {
 
 
 fn main() {
-    let mut cam = Camera {
-        pos: Vector3::new(-3., 0., 0.),
-        screen_size: Vector3::new(4., 4., 0.),
-        screen_resolution: (200, 200),
-        dist_to_screen: 2.,
-        vector_to_screen: Vector3::new(1., 0., 0.),
-        angle_vector_x: Vector3::new(0., 0., 1.),
-    };
+    let mut cam = Camera::new(
+        Vector3::new(-3., 0., 0.),
+        Vector3::new(1., 1., 0.),
+        (50, 50),
+        0.5,
+        Vector3::new(1., 0., 0.),
+        Vector3::new(0., 0., 1.),   
+    );
 
     let light = Vector3::new(0., 0., 2.);
     
@@ -66,24 +66,23 @@ fn main() {
         //sphere.center.z += 0.1;
         if let Event::Input(i) = &event{
             if let Input::Button(j) = i {
-                if let ButtonState::Press = j.state {
-                    cam.move_pos(j.button);
-                }
+                cam.button_handler(j);
             }
         }
+        cam.move_pos();
         for i in 0..cam.screen_resolution.0 {
             for j in 0..cam.screen_resolution.1 {
                 let mut ray = Vector3::new(0., 0., 0.);
                 pixels.put_pixel(i as u32, j as u32, image::Rgba([0, 0, 0, 255]));
-                for _k in 0..50 {
+                for _k in 0..100 {
                     let p = cam.pos+ray;
                     let dist = super_sphere.get_distance(p);
                     if dist < 0.01 {
-                        let a = super_sphere.s.color*((super_sphere.get_normal(cam.pos+ray)*(light-cam.pos-ray).norm()).powf(2.)+0.1);
+                        let a =  super_sphere.s.color*((super_sphere.get_normal(cam.pos+ray)*(light-cam.pos-ray).norm()).powf(1.)+0.1);
                         pixels.put_pixel(i as u32, j as u32, image::Rgba([a.x as u8, a.y as u8, a.z as u8, 255]));
                         break;
                     } 
-                    else if ray.length() > 10. {break;}
+                    else if ray.length() > 20. {break;}
                     ray = ray+(render_vectors[i][j])*dist;
                 }
             }
