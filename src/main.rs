04d11fbb6b0a8d223e3880c50158d6b507ce5537
Anton1320 <56134229+Anton1320@users.noreply.github.com>
2021-03-17 16:46:50 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
     let mut cam = Camera::new(
         Vector3::new(-3., 0., 0.),
         Vector3::new(1., 1., 0.),
-        (50, 50),
+        (300, 300),
         0.5,
         Vector3::new(1., 0., 0.),
         Vector3::new(0., 0., 1.),   
@@ -43,13 +43,13 @@ fn main() {
         color: Vector3::new(0., 0., 255.),
     };
     let super_sphere  = SuperSphere {s:sphere};
-    /*let cube = Box::new
+    let mut cube = Box::new
     (
-        Vector3::new(3.5, 0., 0.),
         Vector3::new(0., 0., 0.),
-        Vector3::new(1.4, 1.4, 1.4),
+        Vector3::new(0., 0., 0.),
+        Vector3::new(1., 1., 1.),
         Vector3::new(255., 255., 1.)
-    );*/
+    );
     //let mut super_cube = SuperCube { cube: cube, sphere: sphere, color: Vector3::new(255., 255., 1.), };
     let render_vectors = cam.get_render_vectors();
     let mut pixels = image::ImageBuffer::from_pixel(cam.screen_resolution.0 as u32, cam.screen_resolution.1 as u32, image::Rgba([0,0,0, 255]));
@@ -64,6 +64,7 @@ fn main() {
         //super_cube.cube.rotation.y += 1./45.;
         //super_cube.cube.rotation.z += 1./35.;
         //sphere.center.z += 0.1;
+        cube.transform.transform_matrix(Vector3::new(0.0, 0., 0.), Vector3::new(0., 0.0, 0.0), Vector3::new(0., 0., 0.));
         if let Event::Input(i) = &event{
             if let Input::Button(j) = i {
                 cam.button_handler(j);
@@ -74,15 +75,15 @@ fn main() {
             for j in 0..cam.screen_resolution.1 {
                 let mut ray = Vector3::new(0., 0., 0.);
                 pixels.put_pixel(i as u32, j as u32, image::Rgba([0, 0, 0, 255]));
-                for _k in 0..100 {
+                for _k in 0..50 {
                     let p = cam.pos+ray;
-                    let dist = super_sphere.get_distance(p);
+                    let dist = cube.get_distance(p);
                     if dist < 0.01 {
-                        let a =  super_sphere.s.color*((super_sphere.get_normal(cam.pos+ray)*(light-cam.pos-ray).norm()).powf(1.)+0.1);
+                        let a =  cube.color*((cube.get_normal(cam.pos+ray)*(light-cam.pos-ray).norm()).powf(2.)+0.1);
                         pixels.put_pixel(i as u32, j as u32, image::Rgba([a.x as u8, a.y as u8, a.z as u8, 255]));
                         break;
                     } 
-                    else if ray.length() > 20. {break;}
+                    else if ray.length() > 10. {break;}
                     ray = ray+(render_vectors[i][j])*dist;
                 }
             }
