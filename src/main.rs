78071970ea9e7dd3b447468c9c88b854b7b6dd72
Anton1320 <56134229+Anton1320@users.noreply.github.com>
2021-04-06 17:42:00 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,8 @@ fn main() {
     );
 
     //let light = Vector3::new(0., -3., 2.);
-    let light = Vector3::new(0., -10., 0.);
-    let mut sphere = Sphere::new(Vector3::new(-3., 0., 0.), Vector3::new(0., 0., 0.), 1.5, Vector3::new(0., 0., 255.));
+    let light = Vector3::new(5., -10., 5.);
+    let mut sphere = Sphere::new(Vector3::new(-3., 0., 0.), Vector3::new(0., 0., 0.), 1.5, Vector3::new(0., 0., 255.), None);
     //let super_sphere  = SuperSphere {s:sphere};
     let mut cube = Box::new
     (
@@ -42,25 +42,22 @@ fn main() {
         Vector3::new(0., 0., 0.),
         Vector3::new(1., 1., 1.),
         Vector3::new(255., 255., 1.),
+        None
     );
 
     let mut plane = Plane {
         y: 5.,
         transform:Transform::new(Vector3::new(0., 0., 0.), Vector3::new(0., 0., 0.), Vector3::new(1., 1., 1.)),
         color: Vector3::new(255., 0., 0.),
-        //children: vec![],
+        children: vec![],
     };
 
     let mut scene = Folder {
-        figures: vec![&mut cube,  &mut plane, &mut sphere],
-        transform: Transform::new(Vector3::new(0., 0., 0.), Vector3::new(0., 0., 0.), Vector3::new(1., 1., 1.)),
+        children: vec![&mut cube,  &mut plane, &mut sphere],
+        transform: Transform::new(Vector3::new(0., 0., 0.), Vector3::new(0., 3.14, 0.), Vector3::new(1., 1., 1.)),
     };
 
     //let mut super_cube = SuperCube { cube: cube, sphere: sphere, color: Vector3::new(255., 255., 1.), };
-    
-    fn foo() {
-        
-    }
 
     let mut pixels = image::ImageBuffer::from_pixel(cam.screen_resolution.0 as u32, cam.screen_resolution.1 as u32, image::Rgba([0,0,0, 255]));
     let mut window: PistonWindow =
@@ -94,8 +91,9 @@ fn main() {
             .unwrap();
         window.draw_2d(&event, |context, graphics, _| {
             clear([1.0; 4], graphics);
-            //scene.figures[0].change_transform(Vector3::new(0.0, 0., 0.), Vector3::new(0.03, 0.06, 0.09), Vector3::new(0., 0.0, 0.0));
-            scene.change_transform(Vector3::new(0., 0., 0.), Vector3::new(0.00, 0.1, 0.), Vector3::new(0., 0.0, 0.0));
+            //scene.children[0].change_transform(Vector3::new(0.0, 0., 0.), Vector3::new(0.03, 0.06, 0.09), Vector3::new(0., 0.0, 0.0));
+            //scene.children[2].change_transform(Vector3::new(0.0, 0., 0.), Vector3::new(0.03, 0.06, 0.09), Vector3::new(0., 0.0, 0.0));
+            scene.change_transform(Vector3::new(0.0, 0., 0.), Vector3::new(0.00, 0.1, 0.), Vector3::new(0., 0.0, 0.0));
             pixels = cam.render(&scene, light);
             
             image(&tex, context.transform, graphics);
